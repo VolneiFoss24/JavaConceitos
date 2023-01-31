7fdfb22205439af9b747eb2ea8e05e34dbe5b077
Volnei Foss <volneifoss24@gmail.com>
2023-01-31 17:41:46 -0300

    finish

--- src/Ternario.java
+++ src/Ternario.java
@@ -1,5 +1,24 @@
-public class Ternario {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Ternario {
+
+    Boolean majority(Integer age){
+        boolean check = (age >= 18);
+        return check;
+    }
+
+    String party () {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite sua idade");
+        int age = input.nextInt();
+        boolean validation = majority(age);
+        String entrance = validation ? "Seja bem vindo a festa!" : "Lamento, você não tem autorização para entrar";
+        return entrance;
+    }
+
+    public static void main(String[] args) {
+        Ternario instance = new Ternario();
+        String party = instance.party();
+        System.out.println(party);
     }
 }
